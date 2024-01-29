074b243f168d90d27f84a6c15fb698ea8f4e5237
ASBcep <asaboritb2324@politecnics.barcelona>
2024-01-29 17:53:32 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "M08 UF1 1.6 FT Fragments"
+include(":app")
